e98f19594aa8d24ccdd1823c4f1a0d06246c0a2b
Bridiro <ale.bridi15@gmail.com>
2025-06-18 01:13:14 +0200

    feat: custom minijinja filter to correcly escape every possible character

--- glyphr-macros/src/renderer.rs
+++ glyphr-macros/src/renderer.rs
@@ -1,13 +1,41 @@
-use minijinja::{Environment, context};
+use minijinja::{Environment, context, Value};
 
 use crate::config::ToFontLoaded;
 use crate::generator::generate_font;
+
+fn rust_char_escape(value: Value) -> Result<String, minijinja::Error> {
+    let s = value.as_str().ok_or_else(|| {
+        minijinja::Error::new(minijinja::ErrorKind::InvalidOperation, "expected string")
+    })?;
+    
+    if s.len() != 1 {
+        return Err(minijinja::Error::new(
+            minijinja::ErrorKind::InvalidOperation, 
+            "expected single character"
+        ));
+    }
+    
+    let ch = s.chars().next().unwrap();
+    let escaped = match ch {
+        '\'' => "\\'".to_string(),
+        '\\' => "\\\\".to_string(),
+        '\n' => "\\n".to_string(),
+        '\r' => "\\r".to_string(),
+        '\t' => "\\t".to_string(),
+        '\0' => "\\0".to_string(),
+        c if c.is_control() => format!("\\u{{{:04x}}}", c as u32),
+        c => c.to_string(),
+    };
+    
+    Ok(escaped)
+}
 
 /// Generates a String containing all the code to write out the macro
 pub fn render<T: ToFontLoaded>(font_config: T) -> String {
     let loaded_fonts = font_config.to_font_loaded();
 
     let mut env = Environment::new();
+    env.add_filter("rust_char_escape", rust_char_escape);
     env.add_template("fonts", include_str!("../templates/fonts.rs.j2"))
         .unwrap();
 
